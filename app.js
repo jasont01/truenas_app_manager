b77d21f1131eb2b5cdc0c8fd7491143ce11abf41
Jason Thompson <jason@jasont.us>
2022-03-17 16:52:21 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import cron from 'cron'
 import 'dotenv/config'
 
 const API_URL = `${process.env.API_URL}/chart/release`
@@ -9,25 +10,33 @@ const config = {
   },
 }
 
-try {
-  const res = await axios.get(API_URL, config)
+const app = async () => {
+  try {
+    const res = await axios.get(API_URL, config)
 
-  res.data.forEach((app) => {
-    if (app.container_images_update_available) {
-      console.log(app.name, 'has updates available')
-      try {
-        const res = await axios.post(
-          `${API_URL}/upgrade`,
-          {
-            release_name: app.name,
-          },
-          config
-        )
-      } catch (err) {
-        console.error(err)
+    res.data.forEach((app) => {
+      if (app.container_images_update_available) {
+        console.log(app.name, 'has updates available')
+        try {
+          axios.post(
+            `${API_URL}/upgrade`,
+            {
+              release_name: app.name,
+            },
+            config
+          )
+        } catch (err) {
+          console.error(err)
+        }
+      } else {
+        console.log(app.name, 'is up to date')
       }
-    }
-  })
-} catch (error) {
-  console.error(error)
+    })
+  } catch (error) {
+    console.error(error)
+  }
 }
+
+const job = new cron.CronJob('0 0 2 * * 6', app, null, true, 'America/New_York')
+
+job.start()
